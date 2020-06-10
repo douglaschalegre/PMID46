3803cf5e83fc8a07af20524b228f78248a83aa0d
Douglas Chalegre <douglas.chalegre@gmail.com>
2020-06-10 11:33:00 -0300

    Creating students index and show

--- server/src/controller/studentsController.ts
+++ server/src/controller/studentsController.ts
@@ -1,20 +1,57 @@
-import {Request, Response} from 'express';
+import { Request, Response } from 'express';
 import knex from '../database/connection';
 
-class collectsController {
-    async index (request: Request, response: Response) {
-        
-        return response.json();
+class studentsController {
+    async index(request: Request, response: Response) {
+        const students = await knex('students')
+            .select('students.name')
+            .select('students.registration')
+            .select('students.email');
+        return response.json(students);
     }
 
-    async show (request: Request, response: Response) {
-       
-        return response.json({});
+    async show(request: Request, response: Response) {
+        const { registration } = request.params;
+        const students = await knex('students').where('registration', registration)
+        .select('students.id')
+        .select('students.name')
+        .select('students.address')
+        .select('students.adressNumber')
+        .select('students.city')
+        .select('students.uf')
+        .select('students.registration')
+        .select('students.phone')
+        return response.json(students);
     }
 
-    async create (request: Request, response: Response) {
-        
-        return response.json()
+    async create(request: Request, response: Response) {
+        const {
+            name,
+            address,
+            adressNumber,
+            city,
+            uf,
+            registration,
+            phone,
+            email
+        } = request.body;
+        const password = registration
+
+        const student = {
+            name,
+            address,
+            adressNumber,
+            city,
+            uf,
+            registration,
+            phone,
+            email,
+            password
+        }
+
+        const insertedStudent = await knex('students').insert(student);
+
+        return response.json(student);
     }
 }
 
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,29 @@
 import express from 'express';
 import {celebrate, Joi} from 'celebrate';
 
-
-
+import StudentsController from './controller/studentsController';
 
 const routes = express.Router();
 
+const studentsController = new StudentsController();
 
+routes.post('/criarAluno',
+ celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      address: Joi.string().required(),
+      adressNumber: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      registration: Joi.string().required(),
+      phone: Joi.string().required(),
+      email: Joi.string().required(),
+   })
+ },{
+   abortEarly: false
+ }) ,studentsController.create);
 
-
+routes.get('/index', studentsController.index);
+routes.get('/index/:registration', studentsController.show);
+ 
 export default routes
