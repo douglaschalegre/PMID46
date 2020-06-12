600ff6f374e24e3061826a5fa5fa30c5c5f52086
Douglas Chalegre <douglas.chalegre@gmail.com>
2020-06-11 23:17:11 -0300

    Pacient model

---
+++ server/src/database/migrations/03_create_pacient.ts
@@ -0,0 +1,19 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    return knex.schema.createTable('pacient', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('specialization').notNullable();
+        table.string('phone').notNullable();
+        table.string('tooth').notNullable();
+        table.string('details');
+        
+        table.integer('created_by').notNullable()
+        .references('id').inTable('collects');
+    });
+}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('students');
+}
