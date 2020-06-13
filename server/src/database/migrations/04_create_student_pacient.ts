414d60e42e51419505cbd18e1188ed85ee3be49c
Douglas Chalegre <douglas.chalegre@gmail.com>
2020-06-13 19:29:05 -0300

    Student pacient table

---
+++ server/src/database/migrations/04_create_student_pacient.ts
@@ -0,0 +1,21 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex){
+    return knex.schema.createTable('student_pacients', table => {
+        table.increments('id').primary();
+      
+        table.integer('student_id').notNullable()
+        .references('id').inTable('students');
+
+        table.integer('pacient_by').notNullable()
+        .references('id').inTable('pacients');
+
+        table.date('appointment').notNullable();
+        table.boolean('confirmed').notNullable();
+        table.boolean('happened').notNullable();
+    });
+}
+
+export async function down(knex: Knex){
+    return knex.schema.dropTable('pacients');
+}
