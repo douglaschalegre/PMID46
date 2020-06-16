ef37598970328a44204606ce15db548bd4a93e9a
Douglas Chalegre <douglas.chalegre@gmail.com>
2020-06-15 21:27:17 -0300

    Drop table error fix

--- server/src/database/migrations/04_create_student_pacient.ts
+++ server/src/database/migrations/04_create_student_pacient.ts
@@ -17,5 +17,5 @@ export async function up(knex: Knex){
 }
 
 export async function down(knex: Knex){
-    return knex.schema.dropTable('pacients');
+    return knex.schema.dropTable('student_pacients');
 }
