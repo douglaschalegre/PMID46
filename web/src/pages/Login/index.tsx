8c62f6f159c9c1308e5781709f2d1975565c7ae1
Douglas Chalegre <douglas.chalegre@gmail.com>
2020-06-22 01:33:11 -0300

    Creating fullhd Login screen

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
 
 import './styles.css'
+import logo from '../../assets/logo.svg';
 
 
 const LandingPage = () => {
 	return(
-		<h1>Login</h1>
+	<div id="page-login">
+		<div className="content">
+			
+			<header>
+				<div className="header">
+					<div>
+						<img src={logo} alt="PMID46" width="48" height="48"/>
+						<h2 className="icons-space">PMID46</h2>
+					</div>
+
+					<div>
+						<Link to="/admin">
+							<FiArrowLeft size={48}/>
+						</Link>
+					</div>
+				</div>
+			</header>
+
+			<main>
+				<div className="backgroud">
+					<div className="container-login-form">
+						<h6>Login</h6>
+						<form>
+							<div>
+								<label>Login</label>
+								<input/>
+							</div>
+							<div>
+								<label>Senha</label>
+								<input/>
+							</div>
+							<button>Entrar</button>
+						</form>
+					</div>
+				</div>
+			</main>
+		</div>
+	</div>
 	)
 }
 
